07da6fe49b3331581584759cded04e208352c573
Sammonster495 <samarthshetty495@gmail.com>
2024-03-05 20:15:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import TeamPage from './components/teampage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./components/homepage";
+import Background from './components/background';
 
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<Background />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="/team" element={<TeamPage />}></Route>
       </Routes>
