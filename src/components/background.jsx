c3a9a8810de9e9aaca0ec5eb248ea678d49a7b28
Sammonster495 <samarthshetty495@gmail.com>
2024-03-12 11:05:11 +0530

    Hosting database on supabase

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -5,7 +5,7 @@ export default function Background(){
     return(
             <div className="relative w-full">
                 <img src="bg-image.jpg" alt="Anime Freak" className="w-full h-2/6 opacity-50 lg:blur-sm md:blur-[3px] sm:blur-[2px] blur-[1px]"/>
-                <img src="logo.jpg" alt="Logo" className="absolute inset-0 m-auto rounded-full lg:h-60 md:h-44 sm:h-24 h-16"/>
+                <img src=".jpg" alt="Logo" className="absolute inset-0 m-auto rounded-full lg:h-60 md:h-44 sm:h-24 h-16"/>
                 <Navbar />
             </div>
     );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Navbar(){
     return(
         <div className="absolute lg:top-12 md:top-10 sm:top-8 top-6 lg:inset-x-16 md:inset-x-14 sm:inset-x-12 inset-x-8 lg:p-4 md:p-3 sm:p-2 p-1 flex justify-between rounded-xl bg-[#0000005c] opacity-75">
-            <p className="text-white lg:text-5xl md:text-3xl sm:text-xl text-xs hover:text-white hover:bg-slate-500 rounded-xl font-bold font-anta">Anime Freak</p>
+            <p className="text-white lg:text-5xl md:text-3xl sm:text-xl text-xs hover:text-white hover:bg-slate-500 rounded-xl font-bold font-anta">AniFreak</p>
             <ul className="flex lg:space-x-20 md:space-x-14 sm:space-x-10 space-x-8">
                 <li className="text-purple-400 hover:text-white hover:bg-slate-500 rounded-xl lg:text-5xl md:text-3xl sm:text-xl text-xs font-bold font-anta" ><Link to="/home">Home</Link></li>
                 <li className="text-purple-400 hover:text-white hover:bg-slate-500 rounded-xl lg:text-5xl md:text-3xl sm:text-xl text-xs font-bold font-anta" >About</li>
