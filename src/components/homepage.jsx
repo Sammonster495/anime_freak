80e1de6314b843eeca2467fc2795ddea30c0e4ca
Athul Bhandary <athulbhandary28@gmail.com>
2024-03-18 15:39:28 +0530

    Minor changes

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   const displayGenres = genres.map((genre) => (
     <Genre key={genre} genre={genre} />
   ));
-{/*
+
   const [fetchError, setFetchError] = useState(null);
   const [animes, setAnimes] = useState(null);
 
@@ -28,21 +28,21 @@ export default function HomePage() {
     };
 
     fetchAnimes();
-  }, []);*/}
+  }, []);
 
   return (
     <div>
       <Background />
       {displayGenres}
-
-      {/*<div>
+      <p className="text-center text-white text-5xl font-bold font-anta uppercase">Top animes</p>
+      <div>
         {fetchError && <p>{fetchError}</p>}
         {animes && animes.length > 0 ? (
           <div className="flex justify-between p-10">
             {animes.map((anime) => (
-              <div key={anime.id} className="border border-black w-96 p-5 text-white text-xl">
-                <p className="text-center mb-5">{anime.eng_title}</p>
-                <img src={anime.image} alt="anime" className="w-[200px] m-auto h-[250px]"/>
+              <div key={anime.id} className="rounded-xl w-96 p-5 text-white text-xl m-5 bg-slate-500 hover:scale-105 transition cursor-pointer font-anta">
+                <p className="text-center mb-5 text-4xl font-bold">{anime.eng_title}</p>
+                <img src={anime.image} alt="anime" className="w-[200px] m-auto h-[250px] rounded-lg"/>
                 <p className="text-center mt-2">{anime.description}</p>
               </div>
             ))}
@@ -50,7 +50,7 @@ export default function HomePage() {
         ) : (
           <p>No users found!</p>
         )}
-        </div>*/}
+        </div>
         </div>
   );
 }
