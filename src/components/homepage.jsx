78740f5980fb301df7ad044ef33fa1a26eab3e14
Sammonster495 <samarthshetty495@gmail.com>
2024-03-30 00:33:56 +0530

    minor changes

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,7 +2,7 @@ import Background from "./background";
 import Genre from "./genre";
 
 export default function HomePage() {
-  const genres = ["Shounen", "Action", "Sports", "Romance", "Fantasy"];
+  const genres = ["Shounen", "Action", "Sports", "Romance", "Isekai"];
   const displayGenres = genres.map((genre) => (
     <Genre key={genre} genre={genre} />
   ));
