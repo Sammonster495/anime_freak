64b6c72371fb0519a1ba5d3edd96f3035b163746
Sammonster495 <samarthshetty495@gmail.com>
2024-05-09 23:50:00 +0530

    minor changes

--- src/components/genre.jsx
+++ src/components/genre.jsx
@@ -3,10 +3,15 @@ import Card from "./card"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Cookies from "js-cookie"
+import { useProfileStore } from "../store"
 
 export default function Genre(props){
     const [animes, setAnimes] = useState(null)
+    const setProfile = useProfileStore(state => state.setData)
     let watch = Cookies.get('watchlist')
+    let s1 = Cookies.get('firstname')
+    let s2 = Cookies.get('lastname')
+    setProfile([s1[0], s2[0]])
     let watchlist
     if (watch && watch.startsWith('j:')) {
         watch = watch.substring(2);
@@ -16,7 +21,7 @@ export default function Genre(props){
     useEffect(() => {
         const fetchAnime = async () => {
             try{
-                const response = await axios.get(`${process.env.REACT_APP_BACKEND}?genre=${props.genre}`, {
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND}/home?genre=${props.genre}`, {
                     withCredentials: true,
                 })
                 setAnimes(response.data)
