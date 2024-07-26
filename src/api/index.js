5ec72f9d2f372eacc6111f0dfb061ce9c6ac7586
Sammonster495 <samarthshetty495@gmail.com>
2024-07-26 09:31:34 +0530

    minor changes

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const {createClient} = require('@supabase/supabase-js');
 
-const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_ANON_KEY);
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
 
 app.use(cors());
 
